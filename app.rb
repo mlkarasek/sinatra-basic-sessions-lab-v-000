bd3af89069fcfec6dd4886f587932c87d5a53eb1
Monica  Karasek <mlkarasek@gmail.com>
2018-08-13 00:56:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable: Sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @item = session[:item]
+    erb :checkout
+  end
 end
